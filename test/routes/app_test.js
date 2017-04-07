6bf7db945f1e89a8481f8e71990c0792c3422988
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-04-07 13:03:19 -0500

    Moved app_test.js

--- test/routes/app_test.js
+++ test/routes/app_test.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 const request = require('supertest');
 
-const app = require('../app');
+const app = require('../../app');
 const api = request(app);
 
 describe("App", () => {
