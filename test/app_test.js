fe046f7ead07ea1eefe30ebc9705a8d14818bcd5
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-04-07 12:23:43 -0500

    Refactored test file supertest code | now  api.get()

--- test/app_test.js
+++ test/app_test.js
@@ -1,12 +1,13 @@
 const expect = require('chai').expect;
 const request = require('supertest');
+
 const app = require('../app');
+const api = request(app);
 
 describe("App", () => {
 
   it("will return starter json", (done) => {
-    request(app)
-    .get('/')
+    api.get('/')
     .end((err, res) => {
       expect(res.status).to.eql(200);
       expect(res.body).to.have.property('title');
