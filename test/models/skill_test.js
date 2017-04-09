fe55c8bab5b710c5be21232004a03c08d09ce21b
Michael Bruce Allen <CodeAmend@Gmail.Com>
2017-04-08 19:05:05 -0500

    Refactored skill_test to test name and level properties

--- test/models/skill_test.js
+++ test/models/skill_test.js
@@ -8,10 +8,17 @@ describe("Skill Model", () => {
   it("should save a skill", (done) => {
     const skill = new Skill({ name: 'Javascript', level: 5 });
     skill.save()
-    .then((skill) => {
+    .then(() => {
       Skill.findOne({ name: 'Javascript' })
       .then((result) => {
-        expect(result._id).to.eql(skill._id);
+        
+        // name property
+        expect(result).to.have.property('name');
+        expect(result.name).to.eql('Javascript');
+        // level property
+        expect(result).to.have.property('level');
+        expect(result.level).to.eql(5);
+
         done();
       });
     });
